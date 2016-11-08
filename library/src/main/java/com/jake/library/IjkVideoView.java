a47a582df3ff95190bdb33a42f9ddacd1f8eb496
jake <903475400@qq.com>
2016-11-08 19:28:14 +0800

    增加声音控制

--- library/src/main/java/com/jake/library/IjkVideoView.java
+++ library/src/main/java/com/jake/library/IjkVideoView.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
+import java.lang.ref.WeakReference;
 import java.util.Map;
 
 import tv.danmaku.ijk.media.player.ISurfaceTextureHost;
@@ -27,42 +28,53 @@ import tv.danmaku.ijk.media.player.ISurfaceTextureHost;
  */
 @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class IjkVideoView extends FrameLayout implements IVideoView {
-    private static final String TAG = IjkVideoView.class.getSimpleName();
-    private TextureRenderView mTextureRenderView;
-    private boolean mOwnSurfaceTexture = false;
-
     public static enum RenderType {
         SURFACE_VIEW, TEXTURE_VIEW;
     }
 
+    private static final String TAG = IjkVideoView.class.getSimpleName();
+    private TextureRenderView mTextureRenderView;
+    private boolean mOwnSurfaceTexture = false;
     private AudioManager mAudioManager;
     private GestureDetector mGestureDetector;
-    private float distanceX;
-    private float distanceY;
     private Context mAppContext;
     private VideoViewImp mVideoViewImp;
     private SurfaceHolder mSurfaceHolder;
     private SurfaceTexture mSurfaceTexture;
     private RenderType mRenderType;
+    private int mMaxVolume;
+    private int mVolume;
+    private float mBrightness;
 
     public IjkVideoView(Context context) {
-        this(context, null);
+        super(context);
+        init(context);
     }
 
     public IjkVideoView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
+        init(context);
     }
 
     public IjkVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
         mAppContext = context.getApplicationContext();
-        mGestureDetector = new GestureDetector(mAppContext, new MyGestureListener());
+        mGestureDetector = new GestureDetector(mAppContext, new VolumeAndBrightnessGestureListener(this));
         mAudioManager = (AudioManager) mAppContext.getSystemService(Context.AUDIO_SERVICE);
         setFocusable(true);
         setFocusableInTouchMode(true);
         requestFocus();
         setClickable(true);
         setRenderType(RenderType.SURFACE_VIEW);
+        mMaxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if (context instanceof Activity) {
+            mBrightness = ((Activity) context).getWindow().getAttributes().screenBrightness;
+        }
     }
 
     public void setRenderType(@NonNull RenderType type) {
@@ -86,8 +98,14 @@ public class IjkVideoView extends FrameLayout implements IVideoView {
                 addRenderView(surfaceRenderView);
                 break;
         }
+        addOtherLayout();
 
+    }
 
+    /**
+     * 子类在这个地方添加其他布局
+     */
+    protected void addOtherLayout() {
     }
 
     private void addRenderView(View view) {
@@ -208,191 +226,6 @@ public class IjkVideoView extends FrameLayout implements IVideoView {
         }
     };
 
-    private void resetLastTouch() {
-        distanceY = 0;
-        distanceX = 0;
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        Log.d("tag", "dispatchTouchEvent event=" + ev.getAction());
-        return super.dispatchTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        Log.d("tag", "onInterceptTouchEvent event=" + ev.getAction());
-        return super.onInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return handleOnTouch(event);
-    }
-
-    private boolean handleOnTouch(MotionEvent event) {
-        Log.d("tag", "onTouchEvent event=" + event.getAction());
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                resetLastTouch();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                handleTouchMove(event);
-                break;
-            case MotionEvent.ACTION_CANCEL:
-            case MotionEvent.ACTION_UP:
-                resetLastTouch();
-                break;
-        }
-//        if (mGestureDetector.onTouchEvent(event))
-//            return true;
-//
-//        // 处理手势结束
-//        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-//            case MotionEvent.ACTION_UP:
-//                endGesture();
-//                break;
-//        }
-
-        return super.onTouchEvent(event);
-    }
-
-    private void handleTouchMove(MotionEvent e) {
-        distanceX += e.getX();
-        distanceY += e.getY();
-        float mOldX = e.getX(), mOldY = e.getY();
-        int y = (int) e.getRawY();
-
-        int windowWidth = getResources().getDisplayMetrics().widthPixels;
-        int windowHeight = getResources().getDisplayMetrics().heightPixels;
-
-        if (mOldX > windowWidth * 4.0 / 5) {// 右边滑动
-            onVolumeSlide((mOldY - y) / windowHeight);
-        } else if (mOldX < windowWidth / 5.0) {// 左边滑动
-            onBrightnessSlide((mOldY - y) / windowHeight);
-        }
-    }
-
-    /**
-     * 手势结束
-     */
-    private void endGesture() {
-        mVolume = -1;
-        mBrightness = -1f;
-
-        // 隐藏
-//        mDismissHandler.removeMessages(0);
-//        mDismissHandler.sendEmptyMessageDelayed(0, 500);
-    }
-
-
-    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        /**
-         * 双击
-         */
-        @Override
-        public boolean onDoubleTap(MotionEvent e) {
-            return true;
-        }
-
-        /**
-         * 滑动
-         */
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2,
-                                float distanceX, float distanceY) {
-            float mOldX = e1.getX(), mOldY = e1.getY();
-            int y = (int) e2.getRawY();
-
-            int windowWidth = getResources().getDisplayMetrics().widthPixels;
-            int windowHeight = getResources().getDisplayMetrics().heightPixels;
-
-            if (mOldX > windowWidth * 4.0 / 5)// 右边滑动
-                onVolumeSlide((mOldY - y) / windowHeight);
-            else if (mOldX < windowWidth / 5.0)// 左边滑动
-                onBrightnessSlide((mOldY - y) / windowHeight);
-
-            return super.onScroll(e1, e2, distanceX, distanceY);
-        }
-    }
-
-    /**
-     * 滑动改变声音大小
-     *
-     * @param percent
-     */
-    private void onVolumeSlide(float percent) {
-        if (mVolume == -1) {
-            mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-            if (mVolume < 0)
-                mVolume = 0;
-
-            // 显示
-//            mOperationBg.setImageResource(R.drawable.video_volumn_bg);
-//            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
-        }
-
-        int index = (int) (percent * mMaxVolume) + mVolume;
-        if (index > mMaxVolume)
-            index = mMaxVolume;
-        else if (index < 0)
-            index = 0;
-
-        // 变更声音
-        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, index, 0);
-
-        // 变更进度条
-//        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
-//        lp.width = findViewById(R.id.operation_full).getLayoutParams().width
-//                * index / mMaxVolume;
-//        mOperationPercent.setLayoutParams(lp);
-    }
-
-    /**
-     * 滑动改变亮度
-     *
-     * @param percent
-     */
-    private void onBrightnessSlide(float percent) {
-        Activity activity = (Activity) getContext();
-        if (mBrightness < 0) {
-            mBrightness = activity.getWindow().getAttributes().screenBrightness;
-            if (mBrightness <= 0.00f)
-                mBrightness = 0.50f;
-            if (mBrightness < 0.01f)
-                mBrightness = 0.01f;
-
-            // 显示
-//            mOperationBg.setImageResource(R.drawable.video_brightness_bg);
-//            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
-        }
-        WindowManager.LayoutParams lpa = activity.getWindow().getAttributes();
-        lpa.screenBrightness = mBrightness + percent;
-        if (lpa.screenBrightness > 1.0f)
-            lpa.screenBrightness = 1.0f;
-        else if (lpa.screenBrightness < 0.01f)
-            lpa.screenBrightness = 0.01f;
-        activity.getWindow().setAttributes(lpa);
-
-//        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
-//        lp.width = (int) (findViewById(R.id.operation_full).getLayoutParams().width * lpa.screenBrightness);
-//        mOperationPercent.setLayoutParams(lp);
-    }
-
-    /**
-     * 最大声音
-     */
-    private int mMaxVolume;
-    /**
-     * 当前声音
-     */
-    private int mVolume = -1;
-    /**
-     * 当前亮度
-     */
-    private float mBrightness = -1f;
-
     @Override
     public void setURI(Uri uri, Map<String, String> headers) {
         mVideoViewImp.setURI(uri, headers);
@@ -471,6 +304,113 @@ public class IjkVideoView extends FrameLayout implements IVideoView {
     @Override
     public int getAudioSessionId() {
         return mVideoViewImp.getAudioSessionId();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector.onTouchEvent(event))
+            return true;
+
+        // 处理手势结束
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                endGesture();
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+
+    /**
+     * 手势结束
+     */
+    private void endGesture() {
+        mVolume = -1;
+        mBrightness = -1f;
+        // 隐藏
+//        mDismissHandler.removeMessages(0);
+//        mDismissHandler.sendEmptyMessageDelayed(0, 500);
+    }
+
+    private static class VolumeAndBrightnessGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private WeakReference<IjkVideoView> mIjkVideoView;
+
+        public VolumeAndBrightnessGestureListener(@NonNull IjkVideoView ijkVideoView) {
+            mIjkVideoView = new WeakReference<IjkVideoView>(ijkVideoView);
+        }
+
+        private IjkVideoView getIjkVideoView() {
+            return mIjkVideoView.get();
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent oldEv, MotionEvent posEv,
+                                float distanceX, float distanceY) {
+            if (distanceY > distanceX) {
+                float mOldX = oldEv.getX(), mOldY = oldEv.getY();
+                int y = (int) posEv.getRawY();
+                int windowWidth = getIjkVideoView().getResources().getDisplayMetrics().widthPixels;
+                int windowHeight = getIjkVideoView().getResources().getDisplayMetrics().heightPixels;
+
+                if (mOldX > windowWidth / 2)// 右边滑动
+                    getIjkVideoView().onVolumeSlide((mOldY - y) / windowHeight);
+                else if (mOldX < windowWidth / 2)// 左边滑动
+                    getIjkVideoView().onBrightnessSlide((mOldY - y) / windowHeight);
+            }
+            return super.onScroll(oldEv, posEv, distanceX, distanceY);
+        }
+    }
+
+    /**
+     * 滑动改变声音大小
+     *
+     * @param percent
+     */
+    private void onVolumeSlide(float percent) {
+        Log.d(TAG, "onVolumeSlide percent=" + percent);
+        int index = (int) (percent * mMaxVolume) + mVolume;
+        if (index > mMaxVolume) {
+            index = mMaxVolume;
+        } else if (index < 0) {
+            index = 0;
+        }
+        // 变更声音
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, index, 0);
+    }
+
+    /**
+     * 滑动改变亮度
+     *
+     * @param percent
+     */
+    private void onBrightnessSlide(float percent) {
+        Log.d(TAG, "onBrightnessSlide percent=" + percent);
+        if (getContext() != null && getContext() instanceof Activity) {
+            Activity activity = (Activity) getContext();
+            if (mBrightness < 0) {
+                mBrightness = activity.getWindow().getAttributes().screenBrightness;
+                if (mBrightness <= 0.00f)
+                    mBrightness = 0.50f;
+                if (mBrightness < 0.01f)
+                    mBrightness = 0.01f;
+                // 显示
+//            mOperationBg.setImageResource(R.drawable.video_brightness_bg);
+//            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
+            }
+            WindowManager.LayoutParams lpa = activity.getWindow().getAttributes();
+            lpa.screenBrightness = mBrightness + percent;
+            if (lpa.screenBrightness > 1.0f)
+                lpa.screenBrightness = 1.0f;
+            else if (lpa.screenBrightness < 0.01f)
+                lpa.screenBrightness = 0.01f;
+            activity.getWindow().setAttributes(lpa);
+
+//        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
+//        lp.width = (int) (findViewById(R.id.operation_full).getLayoutParams().width * lpa.screenBrightness);
+//        mOperationPercent.setLayoutParams(lp);
+        }
     }
 
 
